5ed6b3c0fe1b4f3b9297de0c8fe706c81f8b979c
ayhand <ayhandani@hotmail.com>
2022-02-27 13:29:30 +0100

    p21 wip

---
+++ AdventOfCode/P20.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+	class P20 : Problem
+	{
+		private const char _dark = '.';
+		private const char _light = '#';
+
+		public void SolveA()
+		{
+			var lines = this.ReadInput();
+			var enhancer = lines[0];
+			var pixels = new char[lines.Length - 2, lines[2].Length];
+			for( int i = 0; i < pixels.GetLength(0); i++ )
+			{
+				for( int j = 0; j < pixels.GetLength(1); j++ )
+				{
+					pixels[i, j] = lines.Skip(2 + i).First()[j];
+				}
+			}
+
+			var image = new Image
+			{
+				Pixels = pixels,
+				Padding = _dark,
+			};
+
+			for( int i = 0; i < 50; i++ )
+			{
+				image = this.Iterate(image, enhancer);
+			}
+
+			//this.Print(image);
+
+			var sumLit = image.Pixels.Cast<char>().Count(a => a == _light);
+			Console.WriteLine(sumLit);
+		}
+
+		private Image Iterate(Image prev, string enhancer)
+		{
+			var newPadding = prev.Padding == _dark ? enhancer[0] : enhancer.Last();
+			var nh = prev.Height + 2;
+			var nw = prev.Width + 2;
+			var newPixels = new char[nh, nw];
+			for( int i = 0; i < nh; i++ )
+			{
+				for( int j = 0; j < nw; j++ )
+				{
+					int pi = i - 1;
+					int pj = j - 1;
+					newPixels[i, j] = this.CalcValue(prev, enhancer, pi, pj);
+				}
+			}
+			return new Image
+			{
+				Pixels = newPixels,
+				Padding = newPadding
+			};
+		}
+
+		private char CalcValue(Image prev, string enhancer, int pi, int pj)
+		{
+			var num = 0;
+			for( int i = pi - 1; i <= pi + 1; i++ )
+			{
+				for( int j = pj - 1; j <= pj + 1; j++ )
+				{
+					var c = prev.InBounds(i, j) ? prev.Pixels[i, j] : prev.Padding;
+					num <<= 1;
+					if( c == _light )
+						num += 1;
+				}
+			}
+			return enhancer[num];
+		}
+
+		private void Print(Image i)
+		{
+			this.PrintArray(i.Pixels, a => a.ToString());
+			Console.WriteLine(i.Padding);
+		}
+
+		struct Image
+		{
+			public char[,] Pixels { get; set; }
+			public char Padding { get; set; }
+			public int Height => this.Pixels.GetLength(0);
+			public int Width => this.Pixels.GetLength(1);
+
+			public bool InBounds(int i, int j) => i >= 0 && i < this.Height && j >= 0 && j < this.Width;
+		}
+	}
+}
---
+++ AdventOfCode/P21.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+	class P21 : Problem
+	{
+		public void SolveA()
+		{
+			var player1 = new Player(4);
+			var player2 = new Player(7);
+			var currIs1 = true;
+			var die = new Die();
+
+			while( player1.Score < 1000 && player2.Score < 1000 )
+			{
+				var curr = currIs1 ? player1 : player2;
+				var steps = die.Roll() + die.Roll() + die.Roll();
+				curr.Position += steps;
+				curr.Position = (curr.Position + 9) % 10 + 1;
+				curr.Score += curr.Position;
+				currIs1 = !currIs1;
+			}
+
+			var losingScore = player1.Score < 1000 ? player1.Score : player2.Score;
+			Console.WriteLine(losingScore * die.NumRolls);
+		}
+
+		public void SolveB()
+		{
+			var player1 = new Player(4);
+			var player2 = new Player(7);
+			var nums1 = this.GetNums(player1).ToList();
+			var nums2 = this.GetNums(player2).ToList();
+		}
+
+		private IEnumerable<int> GetNums(Player p)
+		{
+			if( p.Score >= 21 )
+			{
+				yield return p.NumTurns;
+				yield break;
+			}
+
+			for( int i = 1; i <= 3; i++ )
+			{
+				var curr = p;
+				curr.Position += i;
+				curr.Position = (curr.Position + 9) % 10 + 1;
+				curr.Score += curr.Position;
+				curr.NumTurns++;
+				foreach( var num in this.GetNums(curr) )
+				{
+					yield return num;
+				}
+			}
+		}
+
+		struct Player
+		{
+			public Player(int position)
+			{
+				this.Position = position;
+				this.Score = 0;
+				this.NumTurns = 0;
+			}
+
+			public int Score { get; set; }
+			public int Position { get; set; }
+			public int NumTurns { get; set; }
+		}
+
+		class Die
+		{
+			private int _nextRoll = 0;
+			public int NumRolls { get; set; } = 0;
+
+			public int Roll()
+			{
+				this.NumRolls++;
+				var next = _nextRoll++;
+				_nextRoll %= 100;
+				return next + 1;
+			}
+		}
+	}
+}
