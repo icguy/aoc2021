0fd68cc84d480352151a4e6d33a57d1f9975c282
ayhand <ayhandani@hotmail.com>
2022-03-19 00:58:11 +0100

    p21 wip, p22 wip

--- AdventOfCode/P21.cs
+++ AdventOfCode/P21.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,30 +34,55 @@ namespace AdventOfCode
 		{
 			var player1 = new Player(4);
 			var player2 = new Player(7);
-			var nums1 = this.GetNums(player1).ToList();
-			var nums2 = this.GetNums(player2).ToList();
+			var w1 = new BigInteger(0);
+			var w2 = new BigInteger(0);
+			var state = new GameState
+			{
+				IsPlayer1Current = true,
+				P1 = player1,
+				P2 = player2,
+			};
+			this.Iterate(state, ref w1, ref w2);
 		}
 
-		private IEnumerable<int> GetNums(Player p)
+		private void Iterate(GameState state, ref BigInteger w1, ref BigInteger w2)
 		{
-			if( p.Score >= 21 )
+			var p = state.IsPlayer1Current ? state.P1 : state.P2;
+			if( p.Score >= 1000 )
 			{
-				yield return p.NumTurns;
-				yield break;
+				if( state.IsPlayer1Current )
+					w1++;
+				else
+					w2++;
+				return;
 			}
 
-			for( int i = 1; i <= 3; i++ )
-			{
-				var curr = p;
-				curr.Position += i;
-				curr.Position = (curr.Position + 9) % 10 + 1;
-				curr.Score += curr.Position;
-				curr.NumTurns++;
-				foreach( var num in this.GetNums(curr) )
-				{
-					yield return num;
-				}
-			}
+
+			for( int roll1 = 1; roll1 <= 3; roll1++ )
+				for( int roll2 = 1; roll2 <= 3; roll2++ )
+					for( int roll3 = 1; roll3 <= 3; roll3++ )
+					{
+						p.Position += roll1 + roll2 + roll3;
+						p.Position = (p.Position + 9) % 10 + 1;
+						p.Score += p.Position;
+						p.NumTurns++;
+						if( state.IsPlayer1Current )
+							state.P1 = p;
+						else
+							state.P2 = p;
+
+						state.IsPlayer1Current = !state.IsPlayer1Current;
+						this.Iterate(state, ref w1, ref w2);
+					}
+		}
+
+		struct GameState
+		{
+			public Player P1 { get; set; }
+			public Player P2 { get; set; }
+			public bool IsPlayer1Current { get; set; }
+
+			public override string ToString() => $"{(IsPlayer1Current ? "!" : "")}p1=({P1}) {(IsPlayer1Current ? "" : "!")}p2=({P2})";
 		}
 
 		struct Player
@@ -71,6 +97,8 @@ namespace AdventOfCode
 			public int Score { get; set; }
 			public int Position { get; set; }
 			public int NumTurns { get; set; }
+
+			public override string ToString() => $"p{Position} s{Score}";
 		}
 
 		class Die
---
+++ AdventOfCode/P22.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+	internal class P22 : Problem
+	{
+		public void SolveA()
+		{
+			var input = this.ReadInput();
+			var steps = input.Select(i =>
+			{
+				var tokens = i.Split(new string[] { " ", "=", "..", "x", "y", "z", "," }, StringSplitOptions.RemoveEmptyEntries);
+				return new Step
+				{
+					TurnOn = tokens[0] == "on",
+					XMin = int.Parse(tokens[1]),
+					XMax = int.Parse(tokens[2]),
+					YMin = int.Parse(tokens[3]),
+					YMax = int.Parse(tokens[4]),
+					ZMin = int.Parse(tokens[5]),
+					ZMax = int.Parse(tokens[6]),
+				};
+			})
+				.ToList();
+
+			var cubes = new List<Cube>();
+			for( int x = -50; x <= 50; x++ )
+			{
+				for( int y = -50; y <= 50; y++ )
+				{
+					for( int z = -50; z <= 50; z++ )
+					{
+						cubes.Add(new Cube { IsOn = false, X = x, Y = y, Z = z });
+					}
+				}
+			}
+
+			foreach( var step in steps )
+			{
+				foreach( var cube in cubes )
+				{
+					var xOk = cube.X >= step.XMin && cube.X <= step.XMax;
+					var yOk = cube.Y >= step.YMin && cube.Y <= step.YMax;
+					var zOk = cube.Z >= step.ZMin && cube.Z <= step.ZMax;
+					if( xOk && yOk && zOk )
+					{
+						cube.IsOn = step.TurnOn;
+					}
+				}
+			}
+
+			Console.WriteLine(cubes.Count(c => c.IsOn));
+		}
+
+		public void SolveB()
+		{
+			var input = this.ReadInput();
+			var steps = input.Select(i =>
+			{
+				var tokens = i.Split(new string[] { " ", "=", "..", "x", "y", "z", "," }, StringSplitOptions.RemoveEmptyEntries);
+				return new Step
+				{
+					TurnOn = tokens[0] == "on",
+					XMin = int.Parse(tokens[1]),
+					XMax = int.Parse(tokens[2]),
+					YMin = int.Parse(tokens[3]),
+					YMax = int.Parse(tokens[4]),
+					ZMin = int.Parse(tokens[5]),
+					ZMax = int.Parse(tokens[6]),
+				};
+			})
+				.ToList();
+
+			var xLimits = new List<int>();
+			foreach( var step in steps )
+			{
+				xLimits.Add(step.XMin);
+				xLimits.Add(step.XMax + 1);
+			}
+			var yLimits = new List<int>();
+			foreach( var step in steps )
+			{
+				yLimits.Add(step.YMin);
+				yLimits.Add(step.YMax + 1);
+			}
+			var zLimits = new List<int>();
+			foreach( var step in steps )
+			{
+				zLimits.Add(step.ZMin);
+				zLimits.Add(step.ZMax + 1);
+			}
+			xLimits.Sort();
+			yLimits.Sort();
+			zLimits.Sort();
+			var regions = new List<Step>();
+			var numOn = new BigInteger(0);
+			var size = new BigInteger(1);
+			for( int i = 1; i < xLimits.Count; i++ )
+			{
+				Console.WriteLine(i);
+				for( int j = 1; j < yLimits.Count; j++ )
+				{
+					for( int k = 1; k < zLimits.Count; k++ )
+					{
+						var region = new Step
+						{
+							TurnOn = false,
+							XMin = xLimits[i - 1],
+							XMax = xLimits[i] - 1,
+							YMin = yLimits[j - 1],
+							YMax = yLimits[j] - 1,
+							ZMin = zLimits[k - 1],
+							ZMax = zLimits[k] - 1,
+						};
+
+						foreach( var step in steps )
+						{
+							var xOk = region.XMin >= step.XMin && region.XMax <= step.XMax;
+							var yOk = region.YMin >= step.YMin && region.YMax <= step.YMax;
+							var zOk = region.ZMin >= step.ZMin && region.ZMax <= step.ZMax;
+							if( xOk && yOk && zOk )
+							{
+								region.TurnOn = step.TurnOn;
+							}
+						}
+
+						if(region.TurnOn)
+						{
+							size = 1;
+							size *= region.XMax - region.XMin + 1;
+							size *= region.YMax - region.YMin + 1;
+							size *= region.ZMax - region.ZMin + 1;
+							numOn += size;
+						}
+					}
+				}
+			}
+		}
+
+		class Step
+		{
+			public int XMin { get; set; }
+			public int XMax { get; set; }
+			public int YMin { get; set; }
+			public int YMax { get; set; }
+			public int ZMin { get; set; }
+			public int ZMax { get; set; }
+			public bool TurnOn { get; set; }
+		}
+
+		class Cube
+		{
+			public int X { get; set; }
+			public int Y { get; set; }
+			public int Z { get; set; }
+			public bool IsOn { get; set; }
+		}
+
+
+	}
+}
